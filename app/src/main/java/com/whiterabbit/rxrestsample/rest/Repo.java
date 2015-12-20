1c2ed9037e0588df4cb069c062eee6691950c622
Federico Paolinelli <fedepaol@gmail.com>
2015-12-20 16:47:56 +0100

    Added copyright, java bean

--- app/src/main/java/com/whiterabbit/rxrestsample/rest/Repo.java
+++ app/src/main/java/com/whiterabbit/rxrestsample/rest/Repo.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 Federico Paolinelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.whiterabbit.rxrestsample.rest;
 
 import com.google.gson.annotations.SerializedName;
@@ -10,12 +27,89 @@ public class Repo {
         private String avatarUrl;
         @SerializedName("html_url")
         private String htmlUrl;
+
+        public Owner(String login, String id, String avatarUrl, String htmlUrl) {
+            this.login = login;
+            this.id = id;
+            this.avatarUrl = avatarUrl;
+            this.htmlUrl = htmlUrl;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getAvatarUrl() {
+            return avatarUrl;
+        }
+
+        public void setAvatarUrl(String avatarUrl) {
+            this.avatarUrl = avatarUrl;
+        }
+
+        public String getHtmlUrl() {
+            return htmlUrl;
+        }
+
+        public void setHtmlUrl(String htmlUrl) {
+            this.htmlUrl = htmlUrl;
+        }
     }
 
     private String id;
     private String name;
     @SerializedName("full_name")
     private String fullName;
+    private Owner owner;
 
+    public Repo(String id, String name, String fullName, Owner owner) {
+        this.id = id;
+        this.name = name;
+        this.fullName = fullName;
+        this.owner = owner;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
 }
