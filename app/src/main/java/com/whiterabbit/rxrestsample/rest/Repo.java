32fe32717796ffa6600543b97024203e544a9028
Federico Paolinelli <fedepaol@gmail.com>
2015-12-20 16:40:01 +0100

    Removed author comment

--- app/src/main/java/com/whiterabbit/rxrestsample/rest/Repo.java
+++ app/src/main/java/com/whiterabbit/rxrestsample/rest/Repo.java
@@ -2,9 +2,6 @@ package com.whiterabbit.rxrestsample.rest;
 
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by fedepaol on 20/12/15.
- */
 public class Repo {
     public static class Owner {
         private String login;
